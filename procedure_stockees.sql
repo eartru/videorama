31d153b4a603717d031299fa8d16e6c8818c05f5
eartru <estelle.artru@gmail.com>
2018-11-21 14:42:57 +0100

    stored procedures

--- procedure_stockees.sql
+++ procedure_stockees.sql
@@ -11,84 +11,88 @@ GO
 CREATE Procedure GetTopNProducts
 ( @NbProducts int )  
 as  
-begin  
    select TOP(@NbProducts) p.idProduct, p.title, COUNT(rd.idProduct) as quantity
    from product p
    INNER JOIN rentDetail rd on p.idProduct = rd.idProduct 
    GROUP BY p.idProduct, p.title
    ORDER BY COUNT(rd.idProduct) desc
-End
+GO
 
 CREATE Procedure GetProductsByType
 ( @IdType int )  
-as  
-begin  
+as    
 	select * from product p
 	inner join productType pt on p.idType = pt.idType  
 	where p.idType = @IdType
-End
+GO
 
 CREATE Procedure  GetRentByCustomer
 ( @IdCustomer int )  
 as  
-begin  
    Select r.idRent, r.returnBackDate, p.idProduct, p.title, p.picture 
    from rent r
    INNER JOIN rentDetail rd on r.idRent = rd.idRent 
    INNER JOIN product p on rd.idProduct = p.idProduct
    where idCustomer = @IdCustomer AND inProgress = 1
    order by r.idRent
-End
+GO
 
 CREATE Procedure  GetRentProducts
 ( @IdRent int )  
 as  
-begin  
     select p.idProduct, p.title, p.picture
 	from rent r 
 	INNER JOIN rentDetail rd on r.idRent = rd.idRent 
    INNER JOIN product p on rd.idProduct = p.idProduct
     where r.idRent = @IdRent
-End
+GO
 
 CREATE Procedure GetNewProducts
 as  
-begin  
 	Select top 3 idProduct, title, synopsis, picture from product
 	ORDER BY idProduct DESC 
-END
+GO
 
 CREATE Procedure [GetProductByNameAndType]
 ( @IdType int, @Name varchar(50) )  
 as  
-begin  
    select * from product where idType = @IdType
    and title like '%' + @Name + '%'
-End
-
+GO
 
 CREATE Procedure [PutCustomer]
 ( @UserName varchar(50), @FristName varchar(50), @Name varchar(50), @Email varchar(50),  @Password varchar(255), @Adress varchar(255), @PostalCode varchar(5), @Town varchar(50), @Country varchar(50))  
 as  
-begin  
 	DECLARE @new_parent_id INT
 	INSERT INTO videoramaUser (username, passwordUser, email, isAdmin) VALUES ( @UserName, HashBytes('SHA1', @Password), @Email, 'false');	
 	SELECT @new_parent_id = SCOPE_IDENTITY()
 	INSERT INTO customer(idUser, firstname, lastname, addressCustomer, postalCode, town, country) VALUES ( @new_parent_id, @FristName, @Name, @Adress, @PostalCode, @Town, @Country);
-End
+GO
 
 CREATE Procedure [GetUserByUserNameAndPassword]
 ( @UserName varchar(50), @Password varchar(255) )  
-as  
-begin  
+as   
    select * from videoramaUser where username = @UserName
    and passwordUser = HashBytes('SHA1', @Password)
-End
+GO
+
+CREATE Procedure GetCustomers
+as    
+   select c.*, u.email from customer c
+   inner join videoramaUser u on c.idUser = u.idUser 
+GO
+
+CREATE Procedure GetCustomerDetail
+( @IdCustomer int)
+as  
+   select c.*, u.email from customer c
+   inner join videoramaUser u on c.idUser = u.idUser 
+   where c.idUser = @IdCustomer
+GO
 
 CREATE Procedure GetRentDetailsForBill
 ( @IdCustomer int, @IdRent int )  
 as  
-begin  
    select r.idRent, c.firstname, c.lastname, c.addressCustomer, c.postalCode, c.town,
 	c.country, r.rentDate, p.title, p.price  
 	from rent r 
@@ -97,8 +101,6 @@ begin
 	inner join customer c on c.idUser = r.idCustomer
 	where r.idCustomer = @IdCustomer
 	and r.idRent = @IdRent
-End
-
 GO
 
 
