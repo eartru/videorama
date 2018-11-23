7b242ba7843dcbcd40cd23e9403c482912a8f194
eartru <estelle.artru@gmail.com>
2018-11-23 19:27:21 +0100

    Secure access data

--- videorama/Controllers/HomeController.cs
+++ videorama/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 using videorama.ViewModels;
@@ -12,17 +13,40 @@ namespace videorama.Controllers
     {
         public ActionResult Index()
         {
+            string role = "";
+            string id = "";
+            int idUser = 0;
+            bool roleAdmin = false;
+
+            if (Request.IsAuthenticated)
+            {
+                var claimIdentity = User.Identity as ClaimsIdentity;
+
+                if (claimIdentity != null)
+                {
+                    role = claimIdentity.FindFirst(ClaimTypes.Role).Value;
+                    id = claimIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                    idUser = Convert.ToInt32(id);
+                    roleAdmin = Convert.ToBoolean(role);
+                }
+            }
+
             int nbProducts = 5;
             ProductsDb dbProducts = new ProductsDb();
-            int idCustomer = 1;
             RentDb dbRent = new RentDb();
             ModelState.Clear();
             AccueilViewModel vm = new AccueilViewModel
             {
                 Product = dbProducts.GetTopNProducts(nbProducts),
-                NewProducts = dbProducts.GetNewProducts(),
-                Rent = dbRent.GetRentByCustomer(idCustomer),
-        };
+                NewProducts = dbProducts.GetNewProducts()
+            };
+            if (Request.IsAuthenticated)
+            {
+                if(!roleAdmin)
+                {
+                    vm.Rent = dbRent.GetRentByCustomer(idUser);
+                }
+            }
             return View(vm);
         }
 
